109a731ccc03a53870cbf242fcfd07c6f20978c1
aravindhan182 <aravindhvjjay007@gmail.com>
2025-02-25 19:16:33 +0530

    added he project details

--- composeApp/src/wasmJsMain/kotlin/com/aravindh/website/App.kt
+++ composeApp/src/wasmJsMain/kotlin/com/aravindh/website/App.kt
@@ -1,32 +1,90 @@
 package com.aravindh.website
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
 fun App() {
     MaterialTheme {
         val scrollState = rememberScrollState()
+        val sectionPositions = remember { mutableStateMapOf<String, Int>() }
+        val coroutineScope = rememberCoroutineScope()
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color(0xFF17202a)).verticalScroll(scrollState)
+                .background(Color(0xFF17202a))
+                .verticalScroll(scrollState)
         ) {
-            HomePage()
+            HomePage(onNavClick = { section ->
+                coroutineScope.launch {
+                    sectionPositions[section]?.let { scrollState.animateScrollTo(it) }
+                }
+            })
+           Divider(
+                color = Color.Gray,
+                thickness = 1.dp,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+
+            Spacer(modifier = Modifier.height(50.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 56.dp)
+                    .background(Color(0xFF17202a))
+                    .padding(vertical = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.Start,
+                    modifier = Modifier.weight(1f)
+                ) {
+                    AnimatedTexts()
+                }
+                ImageFadeInAnimation(modifier = Modifier.weight(1f))
+            }
             Spacer(Modifier.padding(top = 50.dp))
-            About()
+            Section("ABOUT", sectionPositions) { About() }
             Spacer(Modifier.padding(top = 50.dp))
-            Experience()
+            Section("PROJECTS", sectionPositions) { Projects() }
+            Spacer(Modifier.padding(top = 50.dp))
+            Section("EXPERIENCE", sectionPositions) { Experience() }
+            Spacer(Modifier.padding(top = 50.dp))
+            Section("CONTACT", sectionPositions) {  }
         }
+    }
+}
+@Composable
+fun Section(name: String, sectionPositions: MutableMap<String, Int>, content: @Composable () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .onGloballyPositioned { coordinates ->
+                sectionPositions[name] = coordinates.positionInRoot().y.toInt()
+            }
+    ) {
+        content()
     }
 }
--- composeApp/src/wasmJsMain/kotlin/com/aravindh/website/HomePage.kt
+++ composeApp/src/wasmJsMain/kotlin/com/aravindh/website/HomePage.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -36,13 +38,16 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import aravindhwebsite.composeapp.generated.resources.Res
+import aravindhwebsite.composeapp.generated.resources._19197149
+import aravindhwebsite.composeapp.generated.resources.closeup_hands_business_meeting
+import aravindhwebsite.composeapp.generated.resources.compose_multiplatform
+import aravindhwebsite.composeapp.generated.resources.tracking_icon
 import aravindhwebsite.composeapp.generated.resources.webdev_4d72dbba32efee3890cef9bcacce7aa7
 import kotlinx.coroutines.delay
 import org.jetbrains.compose.resources.painterResource
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun HomePage() {
+fun HomePage(onNavClick: (String) -> Unit) {
     Column(
         horizontalAlignment = Alignment.End
     ) {
@@ -59,66 +64,21 @@ fun HomePage() {
                 fontSize = 48.sp
             )
             Row(modifier = Modifier.padding(top = 16.dp)) {
-                Text(
-                    "HOME",
-                    color = Color.White,
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 16.sp,
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-                Text(
-                    "ABOUT",
-                    color = Color.White,
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 16.sp,
-
+                listOf("HOME", "ABOUT", "EXPERIENCE", "PROJECTS", "CONTACT").forEach { section ->
+                    Text(
+                        text = section,
+                        color = Color.White,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 16.sp,
+                        modifier = Modifier.clickable { onNavClick(section) }
                     )
-                Spacer(modifier = Modifier.width(16.dp))
-                Text(
-                    "EXPERIENCE",
-                    color = Color.White,
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 16.sp,
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-                Text(
-                    "PROJECTS",
-                    color = Color.White,
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 16.sp,
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-                Text(
-                    "CONTACT",
-                    color = Color.White,
-                    fontWeight = FontWeight.SemiBold,
-                    fontSize = 16.sp,
-                )
+                    Spacer(modifier = Modifier.width(16.dp))
+                }
             }
-        }
-        Divider(
-            color = Color.Gray,
-            thickness = 1.dp,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
-        Spacer(Modifier.padding(50.dp))
-        Row( // Use Row to place text and image in the same line
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 56.dp)
-                .background(Color(0xFF17202a)),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Column(
-                horizontalAlignment = Alignment.Start
-            ) {
-                AnimatedTexts()
-            }
-            ImageFadeInAnimation()
-
         }
     }
 }
+
 
 @Composable
 fun AnimatedTexts() {
@@ -189,28 +149,32 @@ fun AnimatedTexts() {
 }
 
 @Composable
-fun ImageFadeInAnimation() {
+fun ImageFadeInAnimation(modifier: Modifier = Modifier) {
     var imageSize by remember { mutableStateOf(0.dp) }
     val targetSize = 100.dp
 
-    LaunchedEffect(key1 = Unit) {
+    LaunchedEffect(Unit) {
         while (imageSize < targetSize) {
-            imageSize += 0.5.dp // Smaller increment for slower reveal
-            delay(50) // Increased delay for slower animation
+            imageSize += 0.5.dp
+            delay(50)
         }
     }
+
     var visible by remember { mutableStateOf(false) }
-    LaunchedEffect(key1 = Unit) {
-        delay(500) // Delay before showing the image
+    LaunchedEffect(Unit) {
+        delay(500)
         visible = true
     }
+
     AnimatedVisibility(visible = visible) {
         Image(
             painter = painterResource(Res.drawable.webdev_4d72dbba32efee3890cef9bcacce7aa7),
-            contentDescription = "Your image description"
+            contentDescription = "Your image description",
+            modifier = modifier.size(500.dp)
         )
     }
 }
+
 
 @Composable
 fun About() {
@@ -373,6 +337,91 @@ fun Experience() {
                 }
             }
         }
+    }
+}
 
+@Composable
+fun Projects() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(
+            "PROJECTS", fontSize = 32.sp, color = Color.White, fontWeight = FontWeight.Bold,
+            textDecoration = TextDecoration.Underline
+        )
+        Spacer(modifier = Modifier.padding(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Column(modifier = Modifier.weight(1f)) {
+
+                Text(
+                    "POS",
+                    fontSize = 24.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+                Image(
+                    painter = painterResource(Res.drawable.closeup_hands_business_meeting),
+                    contentDescription = null,
+                    modifier = Modifier.size(240.dp).align(Alignment.CenterHorizontally))
+                Spacer(modifier = Modifier.padding(16.dp))
+                Text(
+                    "This was my first project in my organization, where I worked on migrating an older version of a POS (Point of Sale) application to a newer, optimized version. Throughout this project, I gained valuable experience in code reusability, performance optimization, and efficient data synchronization.\n" +
+                            "\n" +
+                            "The new version of the app improved overall functionality and user experience, enabling seamless POS operations.This project fully developed by using Jetpack components, some pages in Jetpack Compose, Room database for managing the data, For API integration used Retrofit. One of the key enhancements was optimizing the synchronization process, making it 3x faster than the previous version. Additionally, I contributed to refining the codebase, improving maintainability, and implementing best practices to ensure long-term scalability",
+                    color = Color.White,
+                    modifier = Modifier.align(Alignment.CenterHorizontally).padding(start = 8.dp)
+                )
+            }
+            Spacer(modifier = Modifier.padding(8.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    "SPEND SMART",
+                    fontSize = 24.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+                Spacer(modifier = Modifier.padding(16.dp))
+                Text(
+                    "I developed Spend Smart, a personal finance management app built entirely with Jetpack Compose. This app enables users to allocate budgets, track expenses, and monitor income efficiently, providing a clear financial overview with an intuitive and modern UI. It features budget allocation, expense and income tracking, and real-time insights through visual representations of spending patterns. By working on this project, I deepened my expertise in Jetpack Compose, state management, and database handling, enhancing my ability to develop efficient, scalable, and user-friendly financial applications.",
+                    color = Color.White,
+                    modifier = Modifier.align(Alignment.CenterHorizontally).padding(start = 8.dp)
+                )
+                Image(
+                    painter = painterResource(Res.drawable._19197149),
+                    contentDescription = null,
+                    modifier = Modifier.size(240.dp).align(Alignment.CenterHorizontally))
+            }
+            Spacer(modifier = Modifier.padding(8.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    "REPRESENTATIVE TRACKING",
+                    fontSize = 24.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+                Image(
+                    painter = painterResource(Res.drawable.tracking_icon),
+                    contentDescription = null,
+                    modifier = Modifier.size(240.dp).align(Alignment.CenterHorizontally))
+                Spacer(modifier = Modifier.padding(16.dp))
+                Text(
+                    "I worked on representative tracking system project focused on maintaining and bug fixing. This project was fully developed using Android Jetpack components, Kotlin, Google Maps, and Retrofit for API integration. I was responsible for identifying and resolving critical issues, optimizing performance, and enhancing the overall stability of the application.\n" +
+                            "\n" +
+                            "Through this experience, I gained a deep understanding of large-scale system architecture, real-time location tracking, and API efficiency. Additionally, collaborating with cross-functional teams improved my problem-solving skills and ability to work in a fast-paced environment.",
+                    color = Color.White,
+                    modifier = Modifier.align(Alignment.CenterHorizontally).padding(start = 8.dp)
+                )
+
+            }
+        }
     }
 }
