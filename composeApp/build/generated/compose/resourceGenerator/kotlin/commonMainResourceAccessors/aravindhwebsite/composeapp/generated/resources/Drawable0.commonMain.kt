91be999b5ce97cef981f97436366b954f7f35b0f
aravindh2106 <aravindhan@logicsoft.co.in>
2025-05-07 16:19:58 +0530

    added one drawable

---
+++ composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/aravindhwebsite/composeapp/generated/resources/Drawable0.commonMain.kt
@@ -0,0 +1,38 @@
+@file:OptIn(org.jetbrains.compose.resources.InternalResourceApi::class)
+
+package aravindhwebsite.composeapp.generated.resources
+
+import kotlin.OptIn
+import org.jetbrains.compose.resources.DrawableResource
+
+private object CommonMainDrawable0 {
+  public val compose_multiplatform: DrawableResource by 
+      lazy { init_compose_multiplatform() }
+
+  public val webdev_4d72dbba32efee3890cef9bcacce7aa7: DrawableResource by 
+      lazy { init_webdev_4d72dbba32efee3890cef9bcacce7aa7() }
+}
+
+internal val Res.drawable.compose_multiplatform: DrawableResource
+  get() = CommonMainDrawable0.compose_multiplatform
+
+private fun init_compose_multiplatform(): DrawableResource =
+    org.jetbrains.compose.resources.DrawableResource(
+  "drawable:compose_multiplatform",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/drawable/compose-multiplatform.xml", -1, -1),
+    )
+)
+
+internal val Res.drawable.webdev_4d72dbba32efee3890cef9bcacce7aa7: DrawableResource
+  get() = CommonMainDrawable0.webdev_4d72dbba32efee3890cef9bcacce7aa7
+
+private fun init_webdev_4d72dbba32efee3890cef9bcacce7aa7(): DrawableResource =
+    org.jetbrains.compose.resources.DrawableResource(
+  "drawable:webdev_4d72dbba32efee3890cef9bcacce7aa7",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/drawable/webdev_4d72dbba32efee3890cef9bcacce7aa7.xml", -1, -1),
+    )
+)
---
+++ composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/aravindhwebsite/composeapp/generated/resources/String0.commonMain.kt
@@ -0,0 +1,539 @@
+@file:OptIn(org.jetbrains.compose.resources.InternalResourceApi::class)
+
+package aravindhwebsite.composeapp.generated.resources
+
+import kotlin.OptIn
+import org.jetbrains.compose.resources.StringResource
+
+private object CommonMainString0 {
+  public val about: StringResource by 
+      lazy { init_about() }
+
+  public val about_me: StringResource by 
+      lazy { init_about_me() }
+
+  public val about_me_description: StringResource by 
+      lazy { init_about_me_description() }
+
+  public val contact: StringResource by 
+      lazy { init_contact() }
+
+  public val copyrights: StringResource by 
+      lazy { init_copyrights() }
+
+  public val experience: StringResource by 
+      lazy { init_experience() }
+
+  public val experience_desc: StringResource by 
+      lazy { init_experience_desc() }
+
+  public val experience_year: StringResource by 
+      lazy { init_experience_year() }
+
+  public val home: StringResource by 
+      lazy { init_home() }
+
+  public val iam_aravindh: StringResource by 
+      lazy { init_iam_aravindh() }
+
+  public val jetpack_compose: StringResource by 
+      lazy { init_jetpack_compose() }
+
+  public val kmp: StringResource by 
+      lazy { init_kmp() }
+
+  public val know_about_me: StringResource by 
+      lazy { init_know_about_me() }
+
+  public val kotlin: StringResource by 
+      lazy { init_kotlin() }
+
+  public val ktor: StringResource by 
+      lazy { init_ktor() }
+
+  public val lspl: StringResource by 
+      lazy { init_lspl() }
+
+  public val mobile_developer: StringResource by 
+      lazy { init_mobile_developer() }
+
+  public val my_skills: StringResource by 
+      lazy { init_my_skills() }
+
+  public val name: StringResource by 
+      lazy { init_name() }
+
+  public val pos: StringResource by 
+      lazy { init_pos() }
+
+  public val pos_summary: StringResource by 
+      lazy { init_pos_summary() }
+
+  public val postgres: StringResource by 
+      lazy { init_postgres() }
+
+  public val projects: StringResource by 
+      lazy { init_projects() }
+
+  public val representative_tracking: StringResource by 
+      lazy { init_representative_tracking() }
+
+  public val representative_tracking_summary: StringResource by 
+      lazy { init_representative_tracking_summary() }
+
+  public val resume: StringResource by 
+      lazy { init_resume() }
+
+  public val short_summary: StringResource by 
+      lazy { init_short_summary() }
+
+  public val short_summary_2: StringResource by 
+      lazy { init_short_summary_2() }
+
+  public val social_links: StringResource by 
+      lazy { init_social_links() }
+
+  public val spend_smart: StringResource by 
+      lazy { init_spend_smart() }
+
+  public val spend_smart_summary: StringResource by 
+      lazy { init_spend_smart_summary() }
+
+  public val sqlite: StringResource by 
+      lazy { init_sqlite() }
+
+  public val summary: StringResource by 
+      lazy { init_summary() }
+
+  public val this_website_is_belonging_to: StringResource by 
+      lazy { init_this_website_is_belonging_to() }
+
+  public val xml: StringResource by 
+      lazy { init_xml() }
+}
+
+internal val Res.string.about: StringResource
+  get() = CommonMainString0.about
+
+private fun init_about(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:about", "about",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    236, 21),
+    )
+)
+
+internal val Res.string.about_me: StringResource
+  get() = CommonMainString0.about_me
+
+private fun init_about_me(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:about_me", "about_me",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    207, 28),
+    )
+)
+
+internal val Res.string.about_me_description: StringResource
+  get() = CommonMainString0.about_me_description
+
+private fun init_about_me_description(): StringResource =
+    org.jetbrains.compose.resources.StringResource(
+  "string:about_me_description", "about_me_description",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    10, 196),
+    )
+)
+
+internal val Res.string.contact: StringResource
+  get() = CommonMainString0.contact
+
+private fun init_contact(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:contact", "contact",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    258, 27),
+    )
+)
+
+internal val Res.string.copyrights: StringResource
+  get() = CommonMainString0.copyrights
+
+private fun init_copyrights(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:copyrights", "copyrights",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    286, 50),
+    )
+)
+
+internal val Res.string.experience: StringResource
+  get() = CommonMainString0.experience
+
+private fun init_experience(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:experience", "experience",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    573, 34),
+    )
+)
+
+internal val Res.string.experience_desc: StringResource
+  get() = CommonMainString0.experience_desc
+
+private fun init_experience_desc(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:experience_desc", "experience_desc",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    337, 187),
+    )
+)
+
+internal val Res.string.experience_year: StringResource
+  get() = CommonMainString0.experience_year
+
+private fun init_experience_year(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:experience_year", "experience_year",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    525, 47),
+    )
+)
+
+internal val Res.string.home: StringResource
+  get() = CommonMainString0.home
+
+private fun init_home(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:home", "home",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    608, 20),
+    )
+)
+
+internal val Res.string.iam_aravindh: StringResource
+  get() = CommonMainString0.iam_aravindh
+
+private fun init_iam_aravindh(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:iam_aravindh", "iam_aravindh",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    629, 44),
+    )
+)
+
+internal val Res.string.jetpack_compose: StringResource
+  get() = CommonMainString0.jetpack_compose
+
+private fun init_jetpack_compose(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:jetpack_compose", "jetpack_compose",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    674, 43),
+    )
+)
+
+internal val Res.string.kmp: StringResource
+  get() = CommonMainString0.kmp
+
+private fun init_kmp(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:kmp", "kmp",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    718, 15),
+    )
+)
+
+internal val Res.string.know_about_me: StringResource
+  get() = CommonMainString0.know_about_me
+
+private fun init_know_about_me(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:know_about_me", "know_about_me",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    734, 41),
+    )
+)
+
+internal val Res.string.kotlin: StringResource
+  get() = CommonMainString0.kotlin
+
+private fun init_kotlin(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:kotlin", "kotlin",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    776, 22),
+    )
+)
+
+internal val Res.string.ktor: StringResource
+  get() = CommonMainString0.ktor
+
+private fun init_ktor(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:ktor", "ktor",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    799, 40),
+    )
+)
+
+internal val Res.string.lspl: StringResource
+  get() = CommonMainString0.lspl
+
+private fun init_lspl(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:lspl", "lspl",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    840, 36),
+    )
+)
+
+internal val Res.string.mobile_developer: StringResource
+  get() = CommonMainString0.mobile_developer
+
+private fun init_mobile_developer(): StringResource =
+    org.jetbrains.compose.resources.StringResource(
+  "string:mobile_developer", "mobile_developer",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    877, 48),
+    )
+)
+
+internal val Res.string.my_skills: StringResource
+  get() = CommonMainString0.my_skills
+
+private fun init_my_skills(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:my_skills", "my_skills",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    926, 29),
+    )
+)
+
+internal val Res.string.name: StringResource
+  get() = CommonMainString0.name
+
+private fun init_name(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:name", "name",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    956, 28),
+    )
+)
+
+internal val Res.string.pos: StringResource
+  get() = CommonMainString0.pos
+
+private fun init_pos(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:pos", "pos",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    2146, 15),
+    )
+)
+
+internal val Res.string.pos_summary: StringResource
+  get() = CommonMainString0.pos_summary
+
+private fun init_pos_summary(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:pos_summary", "pos_summary",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    985, 1131),
+    )
+)
+
+internal val Res.string.postgres: StringResource
+  get() = CommonMainString0.postgres
+
+private fun init_postgres(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:postgres", "postgres",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    2117, 28),
+    )
+)
+
+internal val Res.string.projects: StringResource
+  get() = CommonMainString0.projects
+
+private fun init_projects(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:projects", "projects",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    2162, 28),
+    )
+)
+
+internal val Res.string.representative_tracking: StringResource
+  get() = CommonMainString0.representative_tracking
+
+private fun init_representative_tracking(): StringResource =
+    org.jetbrains.compose.resources.StringResource(
+  "string:representative_tracking", "representative_tracking",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    3083, 63),
+    )
+)
+
+internal val Res.string.representative_tracking_summary: StringResource
+  get() = CommonMainString0.representative_tracking_summary
+
+private fun init_representative_tracking_summary(): StringResource =
+    org.jetbrains.compose.resources.StringResource(
+  "string:representative_tracking_summary", "representative_tracking_summary",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    2191, 891),
+    )
+)
+
+internal val Res.string.resume: StringResource
+  get() = CommonMainString0.resume
+
+private fun init_resume(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:resume", "resume",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    3147, 22),
+    )
+)
+
+internal val Res.string.short_summary: StringResource
+  get() = CommonMainString0.short_summary
+
+private fun init_short_summary(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:short_summary", "short_summary",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    3362, 189),
+    )
+)
+
+internal val Res.string.short_summary_2: StringResource
+  get() = CommonMainString0.short_summary_2
+
+private fun init_short_summary_2(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:short_summary_2", "short_summary_2",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    3170, 191),
+    )
+)
+
+internal val Res.string.social_links: StringResource
+  get() = CommonMainString0.social_links
+
+private fun init_social_links(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:social_links", "social_links",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    3552, 36),
+    )
+)
+
+internal val Res.string.spend_smart: StringResource
+  get() = CommonMainString0.spend_smart
+
+private fun init_spend_smart(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:spend_smart", "spend_smart",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    4421, 35),
+    )
+)
+
+internal val Res.string.spend_smart_summary: StringResource
+  get() = CommonMainString0.spend_smart_summary
+
+private fun init_spend_smart_summary(): StringResource =
+    org.jetbrains.compose.resources.StringResource(
+  "string:spend_smart_summary", "spend_smart_summary",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    3589, 831),
+    )
+)
+
+internal val Res.string.sqlite: StringResource
+  get() = CommonMainString0.sqlite
+
+private fun init_sqlite(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:sqlite", "sqlite",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    4457, 22),
+    )
+)
+
+internal val Res.string.summary: StringResource
+  get() = CommonMainString0.summary
+
+private fun init_summary(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:summary", "summary",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    4480, 1255),
+    )
+)
+
+internal val Res.string.this_website_is_belonging_to: StringResource
+  get() = CommonMainString0.this_website_is_belonging_to
+
+private fun init_this_website_is_belonging_to(): StringResource =
+    org.jetbrains.compose.resources.StringResource(
+  "string:this_website_is_belonging_to", "this_website_is_belonging_to",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    5736, 76),
+    )
+)
+
+internal val Res.string.xml: StringResource
+  get() = CommonMainString0.xml
+
+private fun init_xml(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:xml", "xml",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/aravindhwebsite.composeapp.generated.resources/values/strings.commonMain.cvr",
+    5813, 15),
+    )
+)
